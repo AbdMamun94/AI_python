93e118feb85ee03260374b4d427d779fc68be94a
Abdullah Al Mamun <91117135+AbdMamun94@users.noreply.github.com>
2021-10-17 23:36:04 +0200

    Streamlit

---
+++ prcs.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Title")
+
+st.text(" i am here ")
+
+st.header("i am here what do you think")
+
+st.subheader(" i am not here")
+
+st.markdown("i am markable")
+
+st.title("Title")
+
+st.success(" Successful ")
+
+st.warning("this is danger")
+
+st.info("Here is information")
+
+st.error("Shit this is ERROR 404")
+
+st.exception("This is an exception")
+
+st.write(1+2)
