4719b8a98de2f2f0b7d066505b5f60af1879de33
Abdullah Al Mamun <91117135+AbdMamun94@users.noreply.github.com>
2021-10-24 23:24:37 +0200

    Add files via upload

---
+++ csv_pandas.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+import pandas as pd 
+import numpy as np
+
+#df = pd.read.csv("C:\Users\shuvomamun\Desktop\New folder.csv")
+df = pd.read_csv('.csv')
+
+print(df.head())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
